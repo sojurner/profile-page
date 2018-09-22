1e4108d95f1507b5724522f80bc3677f18114111
sojurner <pykim0591@gmail.com>
2018-09-22 05:11:43 -0600

    Create about component with styling files

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <section>
+      {/* <a href={}/> */}
+      <p>
+        I'm an aspiring Software developer, with aspirations to build and
+        develop the next big thing
+      </p>
+    </section>
+  );
+};
