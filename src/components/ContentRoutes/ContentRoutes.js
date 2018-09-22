07c587de48fb1b8c7abea2e664783eb4042d025b
sojurner <pykim0591@gmail.com>
2018-09-22 05:12:42 -0600

    Include Content Route Component

---
+++ src/components/ContentRoutes/ContentRoutes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Projects from '../Projects/Projects';
+// import { Contact } from '../Contact/Contact';
+import { About } from '../../components/About/About';
+
+export const ContentRoutes = () => {
+  return (
+    <Switch>
+      <Route exact path={'/Projects'} component={Projects} />
+      {/* <Route exact path={'/Profile'} component={Contact} /> */}
+      <Route exact path={'/About'} component={About} />
+      {/* <Route path={`/Projects/${}`} render={
+        props => {
+          const {id} = props.match.params
+        } */}
+      }
+    </Switch>
+  );
+};
