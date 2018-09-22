bce1226f9686d6bd3d3c30e2419dfb3f9a493220
sojurner <pykim0591@gmail.com>
2018-09-22 05:11:20 -0600

    Project Data file

---
+++ src/Data/ProjectData.js
@@ -0,0 +1,97 @@
+export const allProjects = [
+  {
+    title: 'Minecraft',
+    projectType: 'Static Compositions',
+    description:
+      'Minecraft-inspired static webpage with emphasis on responsive design',
+    githubURL: 'https://github.com/sojurner/Static-Composition-1',
+    githubPages: 'https://sojurner.github.io/Static-Composition-1/',
+    backgroundImg: '../../Images/MineCraft SS.png'
+  },
+  {
+    title: 'Avatar',
+    description:
+      'Avatar-inspired static webpage with emphasis on responsiveness and card display',
+    projectType: 'Static Compositions',
+    githubURL: 'https://github.com/sojurner/Static-Composition-2',
+    githubPages: 'https://sojurner.github.io/Static-Composition-2/',
+    backgroundImg: '../../Images/AvatarSS.png'
+  },
+  {
+    title: 'Amumu',
+    projectType: 'Static Compositions',
+    description:
+      'League of legends inspired static webpage with emphasis on responsiveness and element spacing',
+    githubURL: 'https://github.com/sojurner/Static-Comp-3',
+    githubPages: 'https://sojurner.github.io/Static-Comp-3/',
+    backgroundImg: '../../Images/AmumuSS.png'
+  },
+  {
+    title: 'Movie-tracker',
+    projectType: 'React/Redux',
+    description: 'Track most recent movies and view their trailers',
+    githubURL: 'https://github.com/sojurner/Movie-Tracker',
+    githubPages: 'asdf',
+    backgroundImg: 'asdfs'
+  },
+  {
+    title: 'Stravant',
+    projectType: 'React/Redux',
+    description:
+      'Checkout out your fitness Activity from Strava and track your Pomodoros',
+    githubURL: 'https://github.com/sojurner/Stravant',
+    githubPages: 'asdf',
+    backgroundImg: 'asdfs'
+  },
+  {
+    title: 'Swapi-box',
+    projectType: 'React',
+    description:
+      'Checkout the characters, vehicles, and planet information from the Star Wars series',
+    githubURL: 'https://github.com/sojurner/swapi-box',
+    githubPages: 'asdf',
+    backgroundImg: 'asdfs'
+  },
+  {
+    title: 'Headcount',
+    projectType: 'React',
+    description:
+      'Checkout out your fitness Activity from Strava and track your Pomodoros',
+    githubURL: 'https://github.com/sojurner/headcount2.0',
+    githubPages: 'asdf',
+    backgroundImg: 'asdfs'
+  },
+  {
+    title: 'Tron',
+    projectType: 'Vanilla JS',
+    description: 'Recreation of Tron game using Object-Oriented Programming',
+    githubURL: 'https://github.com/sojurner/game-time-1',
+    githubPages: 'https://michaelyons.github.io/game-time/',
+    backgroundImg: 'asdfs'
+  },
+  {
+    title: 'Number-guesser',
+    projectType: 'Vanilla JS',
+    description:
+      'Checkout out your fitness Activity from Strava and track your Pomodoros',
+    githubURL: 'https://github.com/sojurner/Number-Guesser',
+    githubPages: 'https://sojurner.github.io/Number-Guesser/',
+    backgroundImg: 'asdfs'
+  },
+  {
+    title: 'Linked-list',
+    projectType: 'Vanilla JS',
+    description: 'Bookmark websites',
+    githubURL: 'https://github.com/sojurner/linked-list',
+    githubPages: 'https://sojurner.github.io/linked-list/',
+    backgroundImg: 'asdfs'
+  },
+  {
+    title: '2Do-box',
+    projectType: 'Vanilla JS',
+    description: 'Save your to-dos and Rate them',
+    githubURL: 'https://github.com/sojurner/2DoBox-Pivot',
+    githubPages: 'https://sojurner.github.io/2DoBox-Pivot/',
+    backgroundImg: 'asdfs'
+  }
+];
