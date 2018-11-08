cffe21fdfdba70f9e6eb00fd7a473c690dec8199
sojurner <pykim0591@gmail.com>
2018-11-08 05:36:59 -0700

    Add route for Home

--- src/components/ContentRoutes/ContentRoutes.js
+++ src/components/ContentRoutes/ContentRoutes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Projects from '../Projects/Projects';
 import Contact from '../Contact/Contact';
-import About from '../../components/About/About';
+import About from '../About/About';
+import { Home } from '../Home/Home';
 
 export const ContentRoutes = () => {
   return (
     <Switch>
+      <Route exact path={`/`} component={Home} />
       <Route exact path={'/Projects'} component={Projects} />
       {/* <Route exact path={'/Profile'} component={Contact} /> */}
       <Route exact path={'/About'} component={About} />
