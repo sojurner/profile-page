70f5e63ed83bb4196566f0772e8a3499fb1cae21
sojurner <pykim0591@gmail.com>
2018-11-08 07:36:33 -0700

    Remove route for contact and add route for home

--- src/components/ContentRoutes/ContentRoutes.js
+++ src/components/ContentRoutes/ContentRoutes.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Projects from '../Projects/Projects';
-import Contact from '../Contact/Contact';
+
 import About from '../About/About';
-import { Home } from '../Home/Home';
+import Home from '../Home/Home';
 
 export const ContentRoutes = () => {
   return (
@@ -12,7 +12,6 @@ export const ContentRoutes = () => {
       <Route exact path={'/Projects'} component={Projects} />
       {/* <Route exact path={'/Profile'} component={Contact} /> */}
       <Route exact path={'/About'} component={About} />
-      <Route exact path={'/Contact'} component={Contact} />
       {/* <Route path={`/Projects/${}`} render={
         props => {
           const {id} = props.match.params
