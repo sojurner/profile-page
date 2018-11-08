dcb95cbb1d6ea4f7b323d3b5397030d05696086c
sojurner <pykim0591@gmail.com>
2018-11-08 07:40:25 -0700

    Implement next, previous and current state

--- src/components/ProjectDetails/ProjectDetails.js
+++ src/components/ProjectDetails/ProjectDetails.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import * as projects from '../../Data/ProjectData';
+import { allProjects } from '../../Data/ProjectData';
 import Modal from 'react-responsive-modal';
 
 import { ProjectModal } from '../ProjectModal/ProjectModal';
 
 import './ProjectDetails.css';
-import { totalmem } from 'os';
+import { CurrentProject } from '../CurrentProject/CurrentProject';
 class ProjectDetails extends Component {
   constructor() {
     super();
     this.state = {
       showDescription: '',
       showModal: '',
-      open: false
+      open: false,
+      currentIndex: 0,
+      prevIndex: null,
+      nextIndex: 1,
+      selectedProjects: null
     };
+  }
+
+  async componentDidMount() {
+    const { projectType } = this.props;
+    const selectedProjects = allProjects.filter(
+      comp => comp.projectType === projectType
+    );
+
+    await this.setState({ selectedProjects: selectedProjects });
+    console.log(this.state.selectedProjects);
   }
 
   handleHover = (event, title) => {
@@ -28,6 +42,7 @@ class ProjectDetails extends Component {
   handleClick = (event, title) => {
     event.stopPropagation();
     this.setState({ showModal: title });
+    console.log(title);
     this.onOpenModal();
   };
 
@@ -39,43 +54,99 @@ class ProjectDetails extends Component {
     this.setState({ open: false });
   };
 
-  render() {
-    const { projectType, redirectLink } = this.props;
-    const matchingType = projects.allProjects.filter(
-      comp => comp.projectType === projectType
-    );
+  selectCurrent = (event, index) => {
+    event.preventDefault();
+    this.setState({
+      currentIndex: index,
+      nextIndex: index + 1,
+      prevIndex: index - 1
+    });
+  };
 
-    return matchingType.map(comp => {
+  goToPrevious = () => {
+    if (this.state.currentIndex > 0) {
+      this.setState(previousState => ({
+        currentIndex: previousState.currentIndex - 1,
+        nextIndex: previousState.currentIndex,
+        prevIndex: previousState.currentIndex - 2
+      }));
+    }
+  };
+
+  goToNext = () => {
+    const { currentIndex, selectedProjects } = this.state;
+    if (currentIndex < selectedProjects.length - 1) {
+      this.setState(previousState => ({
+        currentIndex: previousState.currentIndex + 1,
+        nextIndex: previousState.currentIndex + 2,
+        prevIndex: previousState.currentIndex
+      }));
+    }
+  };
+
+  createBtns = () => {
+    const { selectedProjects, currentIndex } = this.state;
+    return selectedProjects.map((project, index) => {
+      return (
+        <span
+          className={currentIndex === index ? `dot dot-active` : `dot`}
+          onClick={event => this.selectCurrent(event, index)}
+        />
+      );
+    });
+  };
+
+  render() {
+    const { selectedProjects, currentIndex, nextIndex, prevIndex } = this.state;
+    if (selectedProjects) {
       return (
         <div>
-          <div
-            onMouseEnter={event => this.handleHover(event, comp.title)}
-            onMouseLeave={event => this.handleHover(event)}
-            className={`project-card ${comp.title}`}
-            onClick={event => this.handleClick(event, comp.title)}
-          >
-            <img
-              className={`img img-${comp.title}`}
-              src={require(`../../Images/${comp.title}.gif`)}
-              height="200"
-              width="300"
+          <div className={`project-card`}>
+            <div
+              className={
+                !currentIndex
+                  ? `slider-wrapper nav-left`
+                  : `slider-wrapper nav-left nav-left-active right-left-active`
+              }
+              onClick={this.goToPrevious}
             />
-            {this.state.showDescription === comp.title && (
-              <h3 className="click-directions">Click to View</h3>
-            )}
+            <CurrentProject
+              currentIndex={currentIndex}
+              handleHover={this.handleHover}
+              handleClick={this.handleClick}
+              selectedProject={selectedProjects[currentIndex]}
+              nextProject={selectedProjects[nextIndex]}
+              previousProject={selectedProjects[prevIndex]}
+            />
+            <div
+              className={
+                currentIndex < selectedProjects.length - 1
+                  ? `slider-wrapper nav-right nav-right-active right-left-active`
+                  : `slider-wrapper nav-right`
+              }
+              onClick={this.goToNext}
+            />
           </div>
-          {this.state.showModal === comp.title && (
+          {this.createBtns()}
+
+          {this.state.showDescription ===
+            selectedProjects[currentIndex].title && (
+            <h3 className="click-directions">Click to View</h3>
+          )}
+          {this.state.showModal === selectedProjects[currentIndex].title && (
             <Modal open={this.state.open} onClose={this.onCloseModal} center>
               <ProjectModal
-                comp={comp}
+                comp={selectedProjects[currentIndex]}
                 onCloseModal={this.onCloseModal}
-                redirectLink={redirectLink}
+                redirectLink={this.props.redirectLink}
               />
             </Modal>
           )}
         </div>
       );
-    });
+    } else {
+      return <div>ds</div>;
+    }
   }
 }
 
